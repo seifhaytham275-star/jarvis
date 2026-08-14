12239ac84ad71eb40605a0ba17bb81b5aec25167
seifhaytham275-star <seifhaytham275@gmail.com>
2026-08-14 08:23:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
+from streamlit_mic_recorder import speech_to_text
 from groq import Groq
 
 client = Groq(api_key="حط_مفتاح_الـ_Groq_هنا")
-
-st.title("🤖 Jarvis")
+st.title("🐱‍👤 Jarvis")
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -12,7 +12,10 @@ for msg in st.session_state.messages:
     with st.chat_message(msg["role"]):
         st.markdown(msg["content"])
 
-if prompt := st.chat_input("اكتب رسالتك هنا..."):
+audio_text = speech_to_text(language='ar', start_prompt="🎙️ اضغط للتحدث", stop_prompt="جاري الاستماع...", just_once=True)
+prompt = st.chat_input("اكتب رسالتك هنا...") or audio_text
+
+if prompt:
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
@@ -23,6 +26,5 @@ if prompt := st.chat_input("اكتب رسالتك هنا..."):
     )
     reply = response.choices[0].message.content
     st.session_state.messages.append({"role": "assistant", "content": reply})
-    
     with st.chat_message("assistant"):
         st.markdown(reply)
