c06460b722bf1650e6c810a405320551b23f746f
seifhaytham275-star <seifhaytham275@gmail.com>
2026-08-24 01:26:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -16,9 +16,10 @@ with st.sidebar:
     selected_model = st.selectbox(
         "Choose Groq Model:",
         [
+            "openai/gpt-oss-120b",
+            "openai/gpt-oss-20b",
             "llama-3.3-70b-versatile",
-            "llama-3.1-8b-instant",
-            "mixtral-8x7b-32768"
+            "llama-3.1-8b-instant"
         ]
     )
     st.markdown("---")
